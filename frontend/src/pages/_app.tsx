5f480a95138bfceb77e453a795f09367a83dfb33
Amakoye <charlesamakoye@gmail.com>
2024-01-21 15:02:40 +0300

    theme setup and basic page testing

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,41 @@
-import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+// import "styles/styles.css";
+import { ThemeProvider } from "theme";
+import GlobalStyles from "theme/GlobalStyles";
+import createEmotionCache from "theme/createEmotionCache";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
+
+type Props = MyAppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp(props: Props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider>
+        <CssBaseline />
+        <GlobalStyles />
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default MyApp;
