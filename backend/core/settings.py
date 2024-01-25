7656ee4109a3fbd66da5ca29f9fc11666b6c433f
Amakoye <charlesamakoye@gmail.com>
2024-01-25 12:21:26 +0300

    remove Docker file

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "PORT": "5434",
     }
     # "default": {
     #     "ENGINE": "django.db.backends.sqlite3",
